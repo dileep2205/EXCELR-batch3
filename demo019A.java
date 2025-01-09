b8bf82a45bceab2178f591bb34807ca0a3ee108b
dileep2205 <dileepgoud223@gmail.com>
2025-01-09 13:16:11 +0530

    demo019A.java

--- demo019A.java
+++ demo019A.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo019{
+public class demo019A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
