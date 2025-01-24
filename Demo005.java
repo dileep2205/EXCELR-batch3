a7e2c3295c47ecd02fc792713b83280605a1a20f
dileep2205 <dileepgoud223@gmail.com>
2025-01-24 12:34:00 +0530

    Update Demo005.java

--- Demo005.java
+++ Demo005.java
@@ -2,7 +2,7 @@ public class Demo005 {
     public static void main(String[] args) {
         String username = "123456";
         String pwd = "1234";
-        while((username == "TestUser01") && (pwd == "TestPassword01")) {
+        while((username == "123456") && (pwd == "1234")) {
             System.out.println("Hello User");
             break;
         }
